a9aeff1cab677bba704394726377a43891e1784d
AnastasiyaChibis <chibisovanastena@gmail.com>
2022-02-25 17:41:20 +0300

    111

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,18 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    private Timer time;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
